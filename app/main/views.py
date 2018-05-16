1b7e1bc591e75b4774c8c70e0a0d876827cef2b7
Sarah-Marion <devsarahmarion@gmail.com>
2018-05-16 15:55:52 +0300

    added mondays and tuesday contant wk2 flask

--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,13 @@
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, abort
 from.import main
 from ..requests import get_movies, get_movie, search_movie 
-from ..models import Review
-from .forms import ReviewForm
+from ..models import Review, User
+from .forms import ReviewForm, UpdateProfile
+from flask_login import login_required
+from ..import db, photos
 
 
+# Views
 @main.route('/')
 def index():
 
@@ -56,7 +59,9 @@ def search(movie_name):
 
 
 @main.route('/movie/review/new/<int:id>', methods = ['GET', 'POST'])
+@login_required
 def new_review(id):
+
     form = ReviewForm()
     movie = get_movie(id)
 
@@ -73,5 +78,47 @@ def new_review(id):
     return render_template('new_review.html', title = title, review_form = form, movie=movie)
 
 
+@main.route('/user/<uname>/update', methods = ['GET', 'POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
 
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()    
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile', uname=user.username))
+
+        return render_template("profile/profile.html", user = user)
+    return render_template('profile/update.html',form =form)
+
+# @main.route('/user/<uname>/update',methods = ['GET','POST'])
+# @login_required
+# def update_profile(uname):
+#     user = User.query.filter_by(username = uname).first()
+#     if user is None:
+#         abort(404)
+
+    
+
+    
+
+
+@main.route('/user/<uname>/update/pic', methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile', uname=uname))
 
--- start.sh
+++ start.sh
@@ -1,2 +1,6 @@
 export MOVIE_API_KEY = <Your Api key goes here>
 export SECRET_KEY = <Your secret key goes here> 
+
+export MAIL_USERNAME=<Your Email Address>
+export MAIL_PASSWORD=<Your Email Password>
+python3.6 manage.py server
